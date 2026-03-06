6070edea71c6514b2736ba27e5a4766377f66cac
Bhaskar787 <budhabhaskar11@gmail.com>
2026-03-06 15:36:16 +0545

    only login to add to cart error handled

--- src/app/components/ProductCard.jsx
+++ src/app/components/ProductCard.jsx
@@ -1,31 +1,57 @@
 "use client";
+
 import { useCart } from "./CartContext";
 import { useState } from "react";
+import { useAuth } from "./AuthContext";
+import { useRouter } from "next/navigation";
 
 export default function ProductCard({ product }) {
   const { addToCart } = useCart();
+  const { user } = useAuth();
+  const router = useRouter();
+
   const [message, setMessage] = useState("");
+  const [type, setType] = useState(""); // success or error
 
   const handleAdd = () => {
+    // If user not logged in
+    if (!user) {
+      setMessage("Please login to add product");
+      setType("error");
+
+      setTimeout(() => {
+        router.push("/login");
+      }, 1500);
+
+      return;
+    }
+
+    // If user logged in
     addToCart(product._id);
-    setMessage("✅ Product added to cart");
+    setMessage("Product added to cart");
+    setType("success");
 
     setTimeout(() => {
       setMessage("");
+      setType("");
     }, 2000);
   };
 
   return (
     <div className="bg-white shadow rounded-lg p-4 hover:shadow-lg relative">
-      
+
       {message && (
-        <div className="absolute top-2 left-1/2 -translate-x-1/2 bg-green-500 text-white px-3 py-1 rounded text-sm">
+        <div
+          className={`absolute top-2 left-1/2 -translate-x-1/2 text-white px-3 py-1 rounded text-sm
+          ${type === "error" ? "bg-red-500" : "bg-green-500"}`}
+        >
           {message}
         </div>
       )}
 
       <img
         src={product.image}
+        alt={product.name}
         className="h-48 w-full object-cover rounded"
       />
 
